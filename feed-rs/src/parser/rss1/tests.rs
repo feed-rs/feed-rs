3573fc6dd1322cbd50be04aa2965ca30d81674f9
Mark Pritchard <mpritcha@gmail.com>
2020-01-26 20:15:04 +1100

    Generate stable ID for RSS 1.0 items
    RSS 1.0 does not define a unique ID for an item within a feed, but does
    indicate that the URI should be the same as the link.
    
    We now use the first link to generate a hash and use that as a stable
    ID.

--- feed-rs/src/parser/rss1/tests.rs
+++ feed-rs/src/parser/rss1/tests.rs
@@ -20,14 +20,14 @@ fn test_example_1() {
         .description(Text::new("Site description".to_owned()))
         .updated(actual.updated)    // not present in the test data
         .entry(Entry::default()
-            .id(entry0.id.as_ref())             // not present in the test data
-            .updated(entry0.updated)            // not present in the test data
+            .id("e759cc25dc488d95ec723f6553e10a4e")     // hash of the link
+            .updated(entry0.updated)                    // not present in the test data
             .title(Text::new("記事1のタイトル".to_owned()))
             .link(Link::new("記事1のURL".to_owned()))
             .summary(Text::new("記事1の内容".to_owned())))
         .entry(Entry::default()
-            .id(entry1.id.as_ref())             // not present in the test data
-            .updated(entry1.updated)            // not present in the test data
+            .id("2c802e03278c5c9b7c6f3690f69f7570")     // hash of the link
+            .updated(entry1.updated)                    // not present in the test data
             .title(Text::new("記事2のタイトル".to_owned()))
             .link(Link::new("記事2のURL".to_owned()))
             .summary(Text::new("記事2の内容".to_owned())));
@@ -56,13 +56,13 @@ fn test_spec_1() {
             .title("XML.com"))
         .updated(actual.updated)    // not present in the test data
         .entry(Entry::default()
-            .id(entry0.id.as_ref())             // not present in the test data
+            .id("4e02e6b5e0c197983a5347308272fa20")     // hash of the link
             .updated(entry0.updated)            // not present in the test data
             .title(Text::new("Processing Inclusions with XSLT".to_owned()))
             .link(Link::new("http://xml.com/pub/2000/08/09/xslt/xslt.html".to_owned()))
             .summary(Text::new("Processing document inclusions with general XML tools can be\n            problematic. This article proposes a way of preserving inclusion\n            information through SAX-based processing.".to_owned())))
         .entry(Entry::default()
-            .id(entry1.id.as_ref())             // not present in the test data
+            .id("82feaa3d819de1de3e5ed0eb59b53706")     // hash of the link
             .updated(entry1.updated)            // not present in the test data
             .title(Text::new("Putting RDF to Work".to_owned()))
             .link(Link::new("http://xml.com/pub/2000/08/09/rdfdb/index.html".to_owned()))
@@ -91,7 +91,7 @@ fn test_spec_2() {
             .title("Meerkat Powered!"))
         .updated(actual.updated)    // not present in the test data
         .entry(Entry::default()
-            .id(entry0.id.as_ref())             // not present in the test data
+            .id("3c8aa6d4cb85520f531b5a9c08498d04")     // hash of the link
             .updated(entry0.updated)            // not present in the test data
             .title(Text::new("XML: A Disruptive Technology".to_owned()))
             .link(Link::new("http://c.moreover.com/click/here.pl?r123".to_owned()))
