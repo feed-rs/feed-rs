feed-rs/feed-rs

commit 74366acf01c581270f79671dea679551aa17f3e4

Resolve URIs for all Link instances
All instances of Link will use the xml:base spec to resolve their hrefs
where possible.

feed-rs/src/model.rs
@@ -8,6 +8,7 @@ use crate::parser::util::timestamp_rfc2822_lenient;
 #[cfg(test)]
 use crate::parser::util::timestamp_rfc3339_lenient;
 use url::Url;
+use crate::parser::util;
 
 /// Combined model for a syndication feed (i.e. RSS1, RSS 2, Atom, JSON Feed)
 ///
@@ -500,7 +501,7 @@ impl Content {
     }
 
     pub fn src(mut self, url: &str) -> Self {
-        self.src = Some(Link::new(url.to_owned()));
+        self.src = Some(Link::new(url, None));
         self
     }
 }
@@ -594,7 +595,7 @@ impl Image {
     }
 
     pub fn link(mut self, link: &str) -> Self {
-        self.link = Some(Link::new(link.to_owned()));
+        self.link = Some(Link::new(link, None));
         self
     }
 
@@ -633,7 +634,12 @@ pub struct Link {
 }
 
 impl Link {
-    pub(crate) fn new(href: String) -> Link {
+    pub(crate) fn new<S: AsRef<str>>(href: S, base: Option<&Url>) -> Link {
+        let href = match util::parse_uri(href.as_ref(), base) {
+            Some(uri) => uri.to_string(),
+            None => href.as_ref().to_string()
+        };
+
         Link {
             href,
             rel: None,

feed-rs/src/parser/atom/mod.rs
@@ -222,7 +222,7 @@ fn handle_image<R: BufRead>(element: Element<R>) -> Option<Image> {
 fn handle_link<R: BufRead>(element: Element<R>) -> Option<Link> {
     // Always need an href
     element.attr_value("href").map(|href| {
-        let mut link = Link::new(href);
+        let mut link = Link::new(&href, element.xml_base.as_ref());
 
         for attr in element.attributes {
             match attr.name.as_str() {

feed-rs/src/parser/json/mod.rs
@@ -27,9 +27,9 @@ fn convert(jf: JsonFeed) -> Feed {
     // Convert feed level fields
     feed.title = Some(Text::new(jf.title));
 
-    if_some_then(jf.home_page_url, |uri| feed.links.push(Link::new(uri)));
+    if_some_then(jf.home_page_url, |uri| feed.links.push(Link::new(&uri, None)));
 
-    if_some_then(jf.feed_url, |uri| feed.links.push(Link::new(uri)));
+    if_some_then(jf.feed_url, |uri| feed.links.push(Link::new(&uri, None)));
 
     if_some_then(jf.description, |text| feed.description = Some(Text::new(text)));
 
@@ -49,7 +49,7 @@ fn convert(jf: JsonFeed) -> Feed {
 
 // Handles an attachment
 fn handle_attachment(attachment: JsonAttachment) -> Link {
-    let mut link = Link::new(attachment.url);
+    let mut link = Link::new(&attachment.url, None);
 
     link.media_type = Some(attachment.mime_type);
     link.title = attachment.title;
@@ -75,9 +75,9 @@ fn handle_item(ji: JsonItem) -> Entry {
         ..Default::default()
     };
 
-    if_some_then(ji.url, |uri| entry.links.push(Link::new(uri)));
+    if_some_then(ji.url, |uri| entry.links.push(Link::new(uri, None)));
 
-    if_some_then(ji.external_url, |uri| entry.links.push(Link::new(uri)));
+    if_some_then(ji.external_url, |uri| entry.links.push(Link::new(uri, None)));
 
     if_some_then(ji.title, |text| entry.title = Some(Text::new(text)));
 

feed-rs/src/parser/rss1/mod.rs
@@ -69,7 +69,7 @@ fn handle_image<R: BufRead>(element: Element<R>) -> ParseFeedResult<Option<Image
 
             (None, "title") => image.title = child.child_as_text(),
 
-            (None, "link") => if_some_then(child.child_as_text(), |uri| image.link = Some(Link::new(uri))),
+            (None, "link") => if_some_then(child.child_as_text(), |uri| image.link = Some(Link::new(uri, element.xml_base.as_ref()))),
 
             // Nothing required for unknown elements
             _ => {}
@@ -124,7 +124,7 @@ fn handle_item<R: BufRead>(element: Element<R>) -> ParseFeedResult<Option<Entry>
                 body: Some(ce.content),
                 content_type: ce.content_type,
                 length: None,
-                src: ce.src.map(Link::new),
+                src: ce.src.map(|s| Link::new(s, element.xml_base.as_ref())),
             });
         }
     }
@@ -140,7 +140,7 @@ fn handle_item<R: BufRead>(element: Element<R>) -> ParseFeedResult<Option<Entry>
 
 // Handles <link>
 fn handle_link<R: BufRead>(element: Element<R>) -> Option<Link> {
-    element.child_as_text().map(Link::new)
+    element.child_as_text().map(|s| Link::new(s, element.xml_base.as_ref()))
 }
 
 // Handles <title>, <description>

feed-rs/src/parser/rss2/mod.rs
@@ -119,7 +119,7 @@ fn handle_enclosure<R: BufRead>(element: Element<R>) -> Option<MediaObject> {
     for attr in &element.attributes {
         let tag_name = attr.name.as_str();
         match tag_name {
-            "url" => content.url = util::parse_uri(&attr.value, &element),
+            "url" => content.url = util::parse_uri(&attr.value, element.xml_base.as_ref()),
             "length" => content.size = attr.value.parse::<u64>().ok(),
             "type" => if_ok_then_some(attr.value.parse::<Mime>(), |mime| content.content_type = mime),
 
@@ -150,7 +150,7 @@ fn handle_image<R: BufRead>(element: Element<R>) -> ParseFeedResult<Option<Image
 
             (None, "title") => image.title = child.child_as_text(),
 
-            (None, "link") => if_some_then(child.child_as_text(), |uri| image.link = Some(Link::new(uri))),
+            (None, "link") => if_some_then(child.child_as_text(), |uri| image.link = Some(Link::new(uri, element.xml_base.as_ref()))),
 
             (None, "width") => if_some_then(child.child_as_text(), |width| {
                 if let Ok(width) = width.parse::<u32>() {
@@ -181,7 +181,7 @@ fn handle_image<R: BufRead>(element: Element<R>) -> ParseFeedResult<Option<Image
 
 // Handles <content:encoded>
 fn handle_content_encoded<R: BufRead>(element: Element<R>) -> ParseFeedResult<Option<Content>> {
-    let src = element.xml_base.as_ref().map(|xml_base| Link::new(xml_base.to_string()));
+    let src = element.xml_base.as_ref().map(|xml_base| Link::new(xml_base.to_string(), element.xml_base.as_ref()));
 
     Ok(element.children_as_string()?.map(|string| Content {
         body: Some(string),
@@ -256,7 +256,7 @@ fn handle_item<R: BufRead>(element: Element<R>) -> ParseFeedResult<Option<Entry>
 
 // Handles <link>
 fn handle_link<R: BufRead>(element: Element<R>) -> Option<Link> {
-    element.child_as_text().map(Link::new)
+    element.child_as_text().map(|s| Link::new(s, element.xml_base.as_ref()))
 }
 
 // Handles <title>, <description> etc

feed-rs/src/parser/atom/tests.rs
@@ -18,11 +18,11 @@ fn test_example_1() {
             .content_type("text/html"))
         .updated_rfc3339("2005-07-31T12:29:29Z")
         .id("tag:example.org,2003:3")
-        .link(Link::new("http://example.org/".into())
+        .link(Link::new("http://example.org/", None)
             .rel("alternate")
             .media_type("text/html")
             .href_lang("en"))
-        .link(Link::new("http://example.org/feed.atom".into())
+        .link(Link::new("http://example.org/feed.atom", None)
             .rel("self")
             .media_type("application/atom+xml"))
         .rights(Text::new("Copyright (c) 2003, Mark Pilgrim".into()))
@@ -36,10 +36,10 @@ fn test_example_1() {
             .author(Person::new("Mark Pilgrim")
                 .uri("http://example.org/")
                 .email("f8dy@example.com"))
-            .link(Link::new("http://example.org/2005/04/02/atom".into())
+            .link(Link::new("http://example.org/2005/04/02/atom", None)
                 .rel("alternate")
                 .media_type("text/html"))
-            .link(Link::new("http://example.org/audio/ph34r_my_podcast.mp3".into())
+            .link(Link::new("http://example.org/audio/ph34r_my_podcast.mp3", None)
                 .rel("enclosure")
                 .media_type("audio/mpeg")
                 .length(1337))
@@ -64,10 +64,10 @@ fn test_example_2() {
     let expected = Feed::new(FeedType::Atom)
         .id("tag:theregister.co.uk,2005:feed/theregister.co.uk/science/")
         .title(Text::new("The Register - Science".into()))
-        .link(Link::new("https://www.theregister.co.uk/science/headlines.atom".into())
+        .link(Link::new("https://www.theregister.co.uk/science/headlines.atom", None)
             .rel("self")
             .media_type("application/atom+xml"))
-        .link(Link::new("https://www.theregister.co.uk/science/".into())
+        .link(Link::new("https://www.theregister.co.uk/science/", None)
             .rel("alternate")
             .media_type("text/html"))
         .rights(Text::new("Copyright © 2019, Situation Publishing".into()))
@@ -83,7 +83,7 @@ fn test_example_2() {
             .updated_rfc3339("2019-07-31T11:54:28Z")
             .author(Person::new("Richard Speed")
                 .uri("https://search.theregister.co.uk/?author=Richard%20Speed"))
-            .link(Link::new("http://go.theregister.com/feed/www.theregister.co.uk/2019/07/31/orbitbeyond_drops_nasa_moon_contract/".into())
+            .link(Link::new("http://go.theregister.com/feed/www.theregister.co.uk/2019/07/31/orbitbeyond_drops_nasa_moon_contract/", None)
                 .rel("alternate")
                 .media_type("text/html"))
             .title(Text::new("Will someone plz dump our shizz on the Moon, NASA begs as one of the space biz vendors drops out".into())
@@ -95,7 +95,7 @@ fn test_example_2() {
             .updated_rfc3339("2019-07-30T05:41:09Z")
             .author(Person::new("Kieren McCarthy")
                 .uri("https://search.theregister.co.uk/?author=Kieren%20McCarthy"))
-            .link(Link::new("http://go.theregister.com/feed/www.theregister.co.uk/2019/07/30/french_arming_satellites/".into())
+            .link(Link::new("http://go.theregister.com/feed/www.theregister.co.uk/2019/07/30/french_arming_satellites/", None)
                 .rel("alternate")
                 .media_type("text/html"))
             .title(Text::new("Satellites with lasers and machine guns coming! China's new plans? Trump's Space Force? Nope, the French".into())
@@ -116,21 +116,21 @@ fn test_example_3() {
 
     let expected = Feed::new(FeedType::Atom)
         .title(Text::new("The Akamai Blog".into()))
-        .link(Link::new("https://blogs.akamai.com/".into())
+        .link(Link::new("https://blogs.akamai.com/", None)
             .rel("alternate")
             .media_type("text/html"))
         .id("tag:blogs.akamai.com,2019-07-30://2")
         .updated_rfc3339("2019-07-30T15:02:05Z")
         .generator(Generator::new("Movable Type Pro 5.2.13")
             .uri("http://www.sixapart.com/movabletype/"))
-        .link(Link::new("http://feeds.feedburner.com/TheAkamaiBlog".into())
+        .link(Link::new("http://feeds.feedburner.com/TheAkamaiBlog", None)
             .rel("self")
             .media_type("application/atom+xml"))
-        .link(Link::new("http://pubsubhubbub.appspot.com/".into())
+        .link(Link::new("http://pubsubhubbub.appspot.com/", None)
             .rel("hub"))
         .entry(Entry::default()
             .title(Text::new("Time to Transfer Risk: Why Security Complexity & VPNs Are No Longer Sustainable".into()))
-            .link(Link::new("http://feedproxy.google.com/~r/TheAkamaiBlog/~3/NnQEuqRSyug/time-to-transfer-risk-why-security-complexity-vpns-are-no-longer-sustainable.html".into())
+            .link(Link::new("http://feedproxy.google.com/~r/TheAkamaiBlog/~3/NnQEuqRSyug/time-to-transfer-risk-why-security-complexity-vpns-are-no-longer-sustainable.html", None)
                 .rel("alternate")
                 .media_type("text/html"))
             .id("tag:blogs.akamai.com,2019://2.3337")
@@ -165,7 +165,7 @@ fn test_example_4() {
 
     let expected = Feed::new(FeedType::Atom)
         .author(Person::new("ebm-papst"))
-        .link(Link::new("http://www.ebmpapst.com/en/ebmpapst_productnews_atom_feed.xml".into())
+        .link(Link::new("http://www.ebmpapst.com/en/ebmpapst_productnews_atom_feed.xml", None)
             .rel("self")
             .media_type("application/atom+xml"))
         .title(Text::new("ebm-papst product news".into()))
@@ -173,7 +173,7 @@ fn test_example_4() {
         .updated_rfc3339("2019-07-29T09:41:09Z")
         .entry(Entry::default()
             .title(Text::new(" Connection with future".into()))
-            .link(Link::new("https://idt.ebmpapst.com/de/en/idt/campaign/simatic-micro-drive.html".into())
+            .link(Link::new("https://idt.ebmpapst.com/de/en/idt/campaign/simatic-micro-drive.html", None)
                 .rel("alternate"))
             .id("tag:ebmpapst.com,2019-07-17:0310161724098")
             .updated_rfc3339("2019-07-17T03:10:16Z")
@@ -197,14 +197,14 @@ fn test_example_5() {
         .author(Person::new("U.S. Geological Survey")
             .uri("https://earthquake.usgs.gov/"))
         .id("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.atom")
-        .link(Link::new("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.atom".into())
+        .link(Link::new("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.atom", None)
             .rel("self"))
         .icon(Image::new("https://earthquake.usgs.gov/favicon.ico".into()))
         .entry(Entry::default()
             .id("urn:earthquake-usgs-gov:nc:73239366")
             .title(Text::new("M 3.6 - 15km W of Petrolia, CA".into()))
             .updated_rfc3339("2019-07-31T13:07:31.364Z")
-            .link(Link::new("https://earthquake.usgs.gov/earthquakes/eventpage/nc73239366".into())
+            .link(Link::new("https://earthquake.usgs.gov/earthquakes/eventpage/nc73239366", None)
                 .rel("alternate")
                 .media_type("text/html"))
             .summary(Text::new(r#"
@@ -233,12 +233,12 @@ fn test_example_6() {
     let expected = Feed::new(FeedType::Atom)
         .id("tag:github.com,2008:https://github.com/feed-rs/feed-rs/releases")
         .link(
-            Link::new("https://github.com/feed-rs/feed-rs/releases".into())
+            Link::new("https://github.com/feed-rs/feed-rs/releases", None)
                 .rel("alternate")
                 .media_type("text/html"),
         )
         .link(
-            Link::new("https://github.com/feed-rs/feed-rs/releases.atom".into())
+            Link::new("https://github.com/feed-rs/feed-rs/releases.atom", None)
                 .rel("self")
                 .media_type("application/atom+xml"),
         )
@@ -249,7 +249,7 @@ fn test_example_6() {
                 .id("tag:github.com,2008:Repository/90976281/v0.2.0")
                 .updated_rfc3339("2020-01-19T16:08:59+11:00")
                 .link(
-                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/v0.2.0".into())
+                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/v0.2.0", None)
                         .rel("alternate")
                         .media_type("text/html"),
                 )
@@ -276,7 +276,7 @@ fn test_example_6() {
                 .id("tag:github.com,2008:Repository/90976281/0.1.3")
                 .updated_rfc3339("2017-07-07T21:47:46+10:00")
                 .link(
-                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/0.1.3".into())
+                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/0.1.3", None)
                         .rel("alternate")
                         .media_type("text/html"),
                 )
@@ -289,7 +289,7 @@ fn test_example_6() {
                 .id("tag:github.com,2008:Repository/90976281/0.1.1")
                 .updated_rfc3339("2017-06-16T18:49:36+10:00")
                 .link(
-                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/0.1.1".into())
+                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/0.1.1", None)
                         .rel("alternate")
                         .media_type("text/html"),
                 )
@@ -306,7 +306,7 @@ fn test_example_6() {
                 .id("tag:github.com,2008:Repository/90976281/0.1.0")
                 .updated_rfc3339("2017-06-15T16:44:26+10:00")
                 .link(
-                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/0.1.0".into())
+                    Link::new("https://github.com/feed-rs/feed-rs/releases/tag/0.1.0", None)
                         .rel("alternate")
                         .media_type("text/html"),
                 )
@@ -360,7 +360,7 @@ fn test_spec_1() {
     let expected = Feed::new(FeedType::Atom)
         .id("urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6")
         .title(Text::new("Example Feed".into()))
-        .link(Link::new("http://example.org/".into()).rel("alternate"))
+        .link(Link::new("http://example.org/", None).rel("alternate"))
         .updated_rfc3339("2003-12-13T18:30:02Z")
         .author(Person::new("John Doe"))
         .entry(
@@ -369,7 +369,7 @@ fn test_spec_1() {
                 .title(Text::new("Atom-Powered Robots Run Amok".into()))
                 .updated_rfc3339("2003-12-13T18:30:02Z")
                 .summary(Text::new("Some text.".into()))
-                .link(Link::new("http://example.org/2003/12/13/atom03".into()).rel("alternate")),
+                .link(Link::new("http://example.org/2003/12/13/atom03", None).rel("alternate")),
         );
 
     // Check

feed-rs/src/parser/json/tests.rs
@@ -14,8 +14,8 @@ fn test_example_1() {
         .id(&actual.id)                 // not in test content
         .updated(actual.updated)        // not in test content
         .title(Text::new("Daring Fireball".into()))
-        .link(Link::new("https://daringfireball.net/".into()))
-        .link(Link::new("https://daringfireball.net/feeds/json".into()))
+        .link(Link::new("https://daringfireball.net/", None))
+        .link(Link::new("https://daringfireball.net/feeds/json", None))
         .author(Person::new("John Gruber")
             .uri("https://twitter.com/gruber".into()))
         .logo(Image::new("https://daringfireball.net/graphics/apple-touch-icon.png".into()))
@@ -25,8 +25,8 @@ fn test_example_1() {
             .published_rfc3339("2020-01-24T23:46:57Z")
             .updated_rfc3339("2020-01-24T23:46:57Z")
             .id("https://daringfireball.net/linked/2020/01/24/bezos-iphone-x".into())
-            .link(Link::new("https://daringfireball.net/linked/2020/01/24/bezos-iphone-x".into()))
-            .link(Link::new("https://www.nytimes.com/2020/01/22/technology/jeff-bezos-hack-iphone.html".into()))
+            .link(Link::new("https://daringfireball.net/linked/2020/01/24/bezos-iphone-x", None))
+            .link(Link::new("https://www.nytimes.com/2020/01/22/technology/jeff-bezos-hack-iphone.html", None))
             .author(Person::new("John Gruber"))
             .content(Content::default()
                 .body(r#"<p>Good summary from The New York Times. Until this week’s news, I don’t believe we knew what type of phone Bezos was using when he was hacked. Now we know: an iPhone X.</p>"#)
@@ -37,8 +37,8 @@ fn test_example_1() {
             .published_rfc3339("2020-01-21T01:07:00Z")
             .updated_rfc3339("2020-01-21T20:58:36Z")
             .id("https://daringfireball.net/linked/2020/01/20/instagram-for-win95".into())
-            .link(Link::new("https://daringfireball.net/linked/2020/01/20/instagram-for-win95".into()))
-            .link(Link::new("https://www.behance.net/gallery/41023081/Instagram-for-Win95?utm_source=morning_brew".into()))
+            .link(Link::new("https://daringfireball.net/linked/2020/01/20/instagram-for-win95", None))
+            .link(Link::new("https://www.behance.net/gallery/41023081/Instagram-for-Win95?utm_source=morning_brew", None))
             .author(Person::new("John Gruber"))
             .content(Content::default()
                 .body(r#"<p>Delightful work by Petrick Studio.</p>"#)
@@ -62,8 +62,8 @@ fn test_spec_1() {
         .updated(actual.updated)        // not in test content
         .title(Text::new("JSON Feed".into()))
         .description(Text::new("JSON Feed is a pragmatic syndication format for blogs, microblogs, and other time-based content.".into()))
-        .link(Link::new("https://jsonfeed.org/".into()))
-        .link(Link::new("https://jsonfeed.org/feed.json".into()))
+        .link(Link::new("https://jsonfeed.org/", None))
+        .link(Link::new("https://jsonfeed.org/feed.json", None))
         .author(Person::new("Brent Simmons and Manton Reece")
             .uri("https://jsonfeed.org/".into()))
         .entry(Entry::default()
@@ -71,7 +71,7 @@ fn test_spec_1() {
             .id("https://jsonfeed.org/2017/05/17/announcing_json_feed".into())
             .title(Text::new("Announcing JSON Feed".into()))
             .published_rfc3339("2017-05-17T08:02:12-07:00")
-            .link(Link::new("https://jsonfeed.org/2017/05/17/announcing_json_feed".into()))
+            .link(Link::new("https://jsonfeed.org/2017/05/17/announcing_json_feed", None))
             .content(Content::default()
                 .body(r#"<p>We — Manton Reece and Brent Simmons — have noticed that JSON has become the developers’ choice for APIs, and that developers will often go out of their way to avoid XML. JSON is simpler to read and write, and it’s less prone to bugs.</p>
 <p>So we developed JSON Feed, a format similar to <a href="http://cyber.harvard.edu/rss/rss.html">RSS</a> and <a href="https://tools.ietf.org/html/rfc4287">Atom</a> but in JSON. It reflects the lessons learned from our years of work reading and publishing feeds.</p>..."#)

feed-rs/src/parser/mediarss.rs
@@ -111,7 +111,7 @@ fn handle_media_content<R: BufRead>(element: Element<R>, media_obj: &mut MediaOb
 
     for attr in &element.attributes {
         match attr.name.as_str() {
-            "url" => content.url = util::parse_uri(&attr.value, &element),
+            "url" => content.url = util::parse_uri(&attr.value, element.xml_base.as_ref()),
 
             "type" => if_ok_then_some(attr.value.parse::<Mime>(), |v| content.content_type = v),
 

commit 91997e25533af8ff228c8a4761cf59d82d75e8a6

Add generated-feed-id-compat feature which makes generated feed IDs compatible with feed-rs 0.2
These were broken in 74366acf01c581270f79671dea679551aa17f3e4
(model.rs) and 242c993367313ba5ab6a4cf20e81b7aac9c80668 (parser/mod.rs).

feed-rs/Cargo.toml
@@ -21,6 +21,9 @@ readme = "README.md"
 [badges]
 travis-ci = { repository = "feed-rs/feed-rs", branch = "master" }
 
+[features]
+generated-feed-id-compat = []
+
 [dependencies]
 chrono = { version = "0.4.26", default-features = false }
 lazy_static = "1.4.0"

feed-rs/src/model.rs
@@ -633,8 +633,14 @@ pub struct Link {
 impl Link {
     pub(crate) fn new<S: AsRef<str>>(href: S, base: Option<&Url>) -> Link {
         let href = match util::parse_uri(href.as_ref(), base) {
-            Some(uri) => uri.to_string(),
-            None => href.as_ref().to_string(),
+            Some(uri) => {
+                let mut ret: String = uri.into();
+                if cfg!(feature="generated-feed-id-compat") && ret.ends_with('/') && !href.as_ref().ends_with('/') {
+                    ret.pop();
+                }
+                ret
+            }
+            None => href.as_ref().to_string()
         }
         .trim()
         .to_string();

feed-rs/src/parser/mod.rs
@@ -177,8 +177,8 @@ const LINK_HASH_KEY2: u64 = 0x90ee_ca4c_90a5_e228;
 
 // Creates a unique ID from the first link, or a UUID if no links are available
 fn create_id(links: &[model::Link], title: &Option<model::Text>, uri: Option<&str>) -> String {
-    if let Some(link) = links.iter().next() {
-        // Generate a stable ID for this item based on the first link
+    if let Some(link) = links.iter().filter(|l| !cfg!(feature="generated-feed-id-compat") || l.rel.is_none()).next() {
+        // Generate a stable ID for this item based on the first (non-atom) link
         let mut hasher = SipHasher::new_with_keys(LINK_HASH_KEY1, LINK_HASH_KEY2);
         hasher.write(link.href.as_bytes());
         if let Some(title) = title {